c62160f0f2f6df47e224d724e27851dd6ce2f877
Justice Mba <justiholldings@gmail.com>
2020-11-09 14:14:43 +0100

    fix syntax for executing files

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@
 
 # Run this script from janus-gateway dir, on the lightsail inance terminal
 sudo chmod +x install_dependencies.sh
-install_dependencies.sh
+./install_dependencies.sh
 
 sudo chmod +x install_libnice.sh
-install_libnice.sh
+./install_libnice.sh
 
 sudo chmod +x install_libsrtp.sh
-install_libsrtp.sh
+./install_libsrtp.sh
 
 sudo chmod +x install_usrsctp.sh
-install_usrsctp.sh
+./install_usrsctp.sh
 
 sudo chmod +x install_libwebsockets.sh
-install_libwebsockets.sh
+./install_libwebsockets.sh
 
 sudo chmod +x install_janus.sh
-install_janus.sh
+./install_janus.sh
 
 # chmod x+ setup_ssl.sh
 # setup_ssl.sh
